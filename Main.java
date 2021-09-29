50c7911a9f357193a5eeb8fa0d9c58645ed39426
Валерия <90615128+okshinaa@users.noreply.github.com>
2021-09-29 15:43:55 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,71 +7,104 @@ public class Main {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
-        read();
+
+        double x1 = readPointCoordinates("X1=");
+        double y1 = readPointCoordinates("Y1=");
+        double x2 = readPointCoordinates("X2=");
+        double y2 = readPointCoordinates("Y2=");
+        double x3 = readPointCoordinates("X3=");
+        double y3 = readPointCoordinates("Y3=");
+
+        double a = calculateTriangleSide(x1,x2,y1,y2);
+        double b = calculateTriangleSide(x2,x3,y2,y3);
+        double c = calculateTriangleSide(x1,x3,y1,y3);
+
+        boolean flag = doesTriangleExists(a,b,c);
+        if(!flag) {
+            System.out.println("Треугольник c такими координатами вершин не существует");
+            System.exit(0);
+        }
+
+        String typeSides = typeTriangleSides(a,b,c);
+        printSidesAnswer(typeSides);
+
+        double alpha = calculateTriangleCorner(b,c,a);
+        double betta = calculateTriangleCorner(a,c,b);
+        double gamma = calculateTriangleCorner(a,b,c);
+
+        String typeCorner = typeTriangleCorner(alpha,betta,gamma);
+        printCornersAnswer(typeCorner);
     }
 
-    public static void read(){
+    private static double readPointCoordinates(String pointName) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите координаты 1ой точки");
-        System.out.print("x1=");
-        double x1 = scanner.nextDouble();
-        System.out.print("y1=");
-        double y1 = scanner.nextDouble();
-
-        System.out.println("Введите координаты 2ой точки");
-        System.out.print("x2=");
-        double x2 = scanner.nextDouble();
-        System.out.print("y2=");
-        double y2 = scanner.nextDouble();
-
-        System.out.println("Введите координаты 3ой точки");
-        System.out.print("x3=");
-        double x3 = scanner.nextDouble();
-        System.out.print("y3=");
-        double y3 = scanner.nextDouble();
-
-        calculanyions(x1, x2, x3, y1, y2, y3);
+        System.out.print("Введите координату вершины " + pointName);
+        return scanner.nextDouble();
     }
 
-    public static  void calculanyions(double x1, double x2, double x3, double y1, double y2, double y3) {
-        double a = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        double b = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y1, 2));
-        double c = Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2));
-
-        double alpha = (Math.acos((Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c)) * 180) / Math.PI;
-        double betta = (Math.acos((Math.pow(a, 2) + Math.pow(c, 2) - Math.pow(b, 2)) / (2 * a * c)) * 180) / Math.PI;
-        double gamma = (Math.acos((Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b)) * 180) / Math.PI;
-
-        write(a,b,c,alpha,betta,gamma);
+    private static double calculateTriangleSide(double x, double xx, double y, double yy) {
+        return Math.sqrt(Math.pow(xx - x, 2) + Math.pow(yy - y, 2));
     }
 
-       public static void write(double a,double b,double c,double alpha, double betta, double gamma){
-            if (a < (b + c) && b < (a + c) && c < (a + b)) {
-                if (alpha == 90 || betta == 90 || gamma == 90) {
-                    System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f - прямоугольный", a, b, c);
-                    System.out.println();
-                }
-                if (alpha > 90 || betta > 90 || gamma > 90) {
-                    System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f - тупоугольный", a, b, c);
-                } else {
-                    System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f - остроугольный", a, b, c);
-                }
-                System.out.println();
+    private static boolean doesTriangleExists(double a, double b, double c) {
+        return (a < (b + c) && b < (a + c) && c < (a + b));
+    }
 
-                if (a == b && a == c && c == b) {
-                    System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f - равносторонний", a, b, c);
-                    System.out.println();
-                }
-                if (a == b || a == c || b == c) {
-                    System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f - равнобедренный", a, b, c);
-                    System.out.println();
-                } else {
-                    System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f - разносторонний", a, b, c);
-                }
+    private static double calculateTriangleCorner(double sideA, double sideB,double sideC) {
+        return (Math.acos((Math.pow(sideA, 2) + Math.pow(sideB, 2) - Math.pow(sideC, 2)) / (2 * sideA * sideB)) * 180)/Math.PI;
+    }
+
+    private static String typeTriangleSides(double a,double b,double c) {
+        if (a == b && a == c && c == b) {
+             return "equilateral";
+        } else {
+            if (a == b || a == c || b == c) {
+                return "isosceles";
             } else {
-                System.out.printf("Треугольник со сторонами %1$.2f, %2$.2f, %2$.3f не существует", a, b, c);
+                return "versatile";
             }
         }
     }
+
+    private static void printSidesAnswer(String typeSides) {
+        switch (typeSides) {
+            case "equilateral":
+                System.out.println("Треугольник равносторонний");
+                break;
+            case "isosceles":
+                System.out.println("Треугольник равнобедренный");
+                break;
+            case "versatile":
+                System.out.println("Треугольник разносторонний");
+                break;
+        }
+    }
+
+    private static String typeTriangleCorner(double alpha,double betta,double gamma) {
+        if (alpha == 90 || betta == 90 || gamma == 90) {
+            return "right";
+        }else {
+            if (alpha > 90 || betta > 90 || gamma > 90) {
+                return "obtuse";
+            } else {
+                return "acute";
+            }
+        }
+    }
+
+    private static void printCornersAnswer(String typeCorner) {
+        switch (typeCorner) {
+            case "right":
+                System.out.println("Треугольник прямоугольный");
+                break;
+            case "obtuse":
+                System.out.println("Треугольник тупоугольный");
+                break;
+            case "acute":
+                System.out.println("Треугольник остроугольный");
+                break;
+        }
+    }
+}
 
